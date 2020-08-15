a7afdb7653c9b1b788c73250f93df9446d44b19c
Mike Rivera <michaelstephenrivera@gmail.com>
2020-08-15 14:11:03 -0700

    Completed chunked

---
+++ chunk/test.js
@@ -0,0 +1,67 @@
+const options = require('./index');
+
+test('function chunk exists', () => {
+  expect(typeof options.chunk).toEqual('function');
+});
+
+test('chunk divides an array of 10 elements with chunk size 2', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const chunked = options.chunk(arr, 2);
+
+  expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
+});
+
+test('chunk divides an array of 3 elements with chunk size 1', () => {
+  const arr = [1, 2, 3];
+  const chunked = options.chunk(arr, 1);
+
+  expect(chunked).toEqual([[1], [2], [3]]);
+});
+
+test('chunk divides an array of 5 elements with chunk size 3', () => {
+  const arr = [1, 2, 3, 4, 5];
+  const chunked = options.chunk(arr, 3);
+
+  expect(chunked).toEqual([[1, 2, 3], [4, 5]]);
+});
+
+test('chunk divides an array of 13 elements with chunk size 5', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const chunked = options.chunk(arr, 5);
+
+  expect(chunked).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]);
+});
+
+// ========================================
+// Solution Two
+test('function chunk exists', () => {
+  expect(typeof options.chunkTwo).toEqual('function');
+});
+
+test('chunk divides an array of 10 elements with chunk size 2', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const chunked = options.chunkTwo(arr, 2);
+
+  expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
+});
+
+test('chunk divides an array of 3 elements with chunk size 1', () => {
+  const arr = [1, 2, 3];
+  const chunked = options.chunkTwo(arr, 1);
+
+  expect(chunked).toEqual([[1], [2], [3]]);
+});
+
+test('chunk divides an array of 5 elements with chunk size 3', () => {
+  const arr = [1, 2, 3, 4, 5];
+  const chunked = options.chunkTwo(arr, 3);
+
+  expect(chunked).toEqual([[1, 2, 3], [4, 5]]);
+});
+
+test('chunk divides an array of 13 elements with chunk size 5', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const chunked = options.chunkTwo(arr, 5);
+
+  expect(chunked).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]);
+});
